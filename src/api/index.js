7370b282b3da87d731d74c4c8948d23378f64cf1
Tim Davis <thebearingedge@gmail.com>
2016-05-13 00:36:43 -0700

    api notes

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,9 @@ import { createUser } from './users'
 import { authenticate } from './authenticate'
 import { issueToken, setUser } from './tokens'
 import { knex, redis } from './core'
-import errorHandler from './error-handler'
 import { vote } from './vote'
 import { getPoll } from './polls'
+import errorHandler from './error-handler'
 
 
 const api = express()
@@ -17,9 +17,13 @@ api
   .use(json())
   .post('/signup', createUser(knex), issueToken(redis))
   .post('/authenticate', authenticate(knex), issueToken(redis))
-  .post('/:optionId', vote(knex))
+  .post('/vote/:optionId', vote(knex))
   .get('/:username/:slug', getPoll(knex))
-  .get('/', setUser(redis), (req, res) => res.json(req.user))
+  .use(setUser(redis))
+  // POST /polls -> createPoll
+  // DELETE /polls/:slug -> deletePoll
+  // GET /:username/polls -> listPolls
   .use(errorHandler)
+
 
 api.listen(3000, _ => console.log('listening on 3000'))
