6dbada299277dce9402f4bb63e8b29168d476f21
Tim Davis <thebearingedge@gmail.com>
2016-05-15 20:17:52 -0700

    add unique option value per poll

---
+++ src/migrations/20160515200110_unique-options_value_poll_id.js
@@ -0,0 +1,15 @@
+
+export const up = ({ schema }) =>
+
+  schema
+    .table('options', tb => {
+      tb.unique(['value', 'poll_id'])
+    })
+
+
+export const down = ({ schema }) =>
+
+  schema
+    .table('option', tb => {
+      tb.dropUnique(['value', 'poll_id'])
+    })
