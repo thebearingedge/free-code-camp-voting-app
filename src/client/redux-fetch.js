18a2781b141208b6cd2b9bdc7896e35ae31dec82
Tim Davis <thebearingedge@gmail.com>
2016-05-21 19:01:03 -0700

    start redux fetch middleware

---
+++ src/client/redux-fetch.js
@@ -0,0 +1,4 @@
+
+export default function reduxFetch(fetch, handlers) {
+
+}
--- src/client/unit/redux-fetch.test.js
+++ src/client/unit/redux-fetch.test.js
@@ -1,8 +1,11 @@
 
-import { expect } from '@thebearingedge/test-utils'
+import { expect, stub } from '@thebearingedge/test-utils'
+import { createStore, applyMiddleware } from 'redux'
+import reduxFetch from '../redux-fetch'
 
 
 describe('redux-fetch', () => {
 
+  it('runs tests!', () => expect(reduxFetch).to.be.a('function'))
 
 })
